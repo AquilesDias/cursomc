91a5a90dab725c9a2d48254c44087b5fe446538e
AquilesDias <kilesax@gmail.com>
2021-08-06 21:49:13 -0300

    Refresh token

---
+++ src/main/java/com/aquilesd/coursemc/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.aquilesd.coursemc.resources;
+
+import com.aquilesd.coursemc.security.JWTUtil;
+import com.aquilesd.coursemc.security.UserSS;
+import com.aquilesd.coursemc.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+
+    @Autowired
+    JWTUtil jwtUtil;
+
+    @RequestMapping(value="/refresh_token", method= RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
